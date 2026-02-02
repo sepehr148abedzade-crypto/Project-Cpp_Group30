8298d1031c5b08d2dd73fd31afad216a74ac35f4
Amirali <amirisalam.3274@gmail.com>
2026-02-02 11:53:18 -0800

    Amirali is here in git

--- Welcometeamate.cpp
+++ Welcometeamate.cpp
@@ -2,5 +2,6 @@
 
 int main(){
     std::cout << "hello my name is Sepehr" << std::endl;
-    //please print your name like me :)
+    std :: cout << "hello my name is Amirali" << std::endl;
+    //please print your name like me and me{amirali} :)
 }
