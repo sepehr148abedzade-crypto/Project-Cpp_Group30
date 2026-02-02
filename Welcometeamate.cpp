7c811e5249d9c52aed8cbc9fc22e5668f3d23e5b
Amirali <amirisalam.3274@gmail.com>
2026-02-02 12:15:16 -0800

    START SDL

--- Welcometeamate.cpp
+++ Welcometeamate.cpp
@@ -1,7 +1,35 @@
+#define SDL_MAIN_HANDLED
 #include <iostream>
+#include <SDL2/SDL.h>
 
-int main(){
+int main( int argc, char *argv[] ){
     std::cout << "hello my name is Sepehr" << std::endl;
     std :: cout << "hello my name is Amirali" << std::endl;
     //please print your name like me and me{amirali} :)
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+    SDL_Window * W1 = SDL_CreateWindow(
+        "WINDOW 1",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        400,300,
+        SDL_WINDOW_SHOWN
+    );
+    if (W1 == nullptr) {
+        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+    bool quit = false;
+    SDL_Event e;
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+    return 0;
 }
